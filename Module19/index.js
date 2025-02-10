e8f6d2a0e05e63ae198bd71cb8d47a9f15b0b898
palsourov <palsourov365@gmail.com>
2025-02-10 08:33:21 +0600

    update module 19

--- Module19/index.js
+++ Module19/index.js
@@ -1,6 +1,32 @@
-const number=[1,2,3,4,5]
-for(const num of number){
+// const number=[1,2,3,4,5]
+// for(const num of number){
     
-    console.log(num);
-}
-console.log(number)
+//     console.log(num);
+// }
+// console.log(number)
+
+// while loop
+// let n = 0;
+// while (n<10) {
+//   console.log(n);
+//   n++;
+    
+// }
+// number of odd 
+// let n=1;
+// let sum=0
+// while ( n<=10) {
+
+//     // sum=sum+n
+//     console.log(n)
+//     console.log('sum' , sum);
+//     n++;
+    
+// }
+// task 1
+  // const n= 60;
+  // for(i=1;i<=60;i++){
+  //   console.log("I will invest at least 6 hrs every single day for next 60 days!");
+  // }
+  // task 2
+  
