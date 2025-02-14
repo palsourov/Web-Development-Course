26476d3d512400470c961523cc7fa6b3848aec0f
palsourov <palsourov365@gmail.com>
2025-02-14 10:43:27 +0600

    update module 20

---
+++ Module20/reverse.js
@@ -0,0 +1,8 @@
+const sen='HTML element'
+// let rev='';
+// for(const letter of sen){
+//     rev=letter + rev;
+// }
+// console.log(rev);
+let rev =sen.split('').reverse().join('')
+console.log(rev);
--- Module20/string.js
+++ Module20/string.js
@@ -1,2 +1,4 @@
- s ='sourov';
-console.log(s);
+// const s ='sourov';
+// console.log(s.toLowerCase());
+// console.log(s.toUpperCase());
+// slice
