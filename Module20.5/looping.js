e556c8a6a98885311b0a68ee246f1adac368e886
palsourov <palsourov365@gmail.com>
2025-02-15 12:42:50 +0600

    added 20.5 module

---
+++ Module20.5/looping.js
@@ -0,0 +1,16 @@
+const name=['sourov','proyash','rajib','rahi'];
+// for(const friends of name){
+//     console.log(friends);
+    
+// }
+// for(i=0;i<name.length;i++){
+//     console.log(i);  
+//     console.log(name[i]);
+    
+// }
+let i=0;
+while(i<name.length){
+    console.log(name[i]);
+     i++;
+    
+}
---
+++ Module20.5/reverse.js
@@ -0,0 +1,12 @@
+const array=[1,2,3,4,5];
+// console.log(array);
+// const rev=array.reverse();
+// console.log(rev);
+const arrays=[];
+for(const arr of array){
+   arrays.unshift(arr)  
+}
+console.log(arrays);
+
+
+
