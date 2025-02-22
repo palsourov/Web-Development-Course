c0751f8de3549a6f6c597d480486bd85b0285527
palsourov <palsourov365@gmail.com>
2025-02-22 18:17:03 +0600

    added module25

---
+++ Module25/js/script.js
@@ -0,0 +1,11 @@
+const lilists = document.getElementsByTagName('li');
+ 
+for(const lilist of lilists){
+    console.log(lilist.innerText);
+    
+}
+const headings = document.getElementsByTagName('h1');
+for(const heading of headings ){
+    console.log(heading.innerText);
+    
+}
