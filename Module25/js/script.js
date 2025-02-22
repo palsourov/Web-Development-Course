eeaf0c5be7b0f58a0c31d1a9622fd1015e69abc8
palsourov <palsourov365@gmail.com>
2025-02-22 19:52:00 +0600

    Update module25

--- Module25/js/script.js
+++ Module25/js/script.js
@@ -1,11 +1,36 @@
-const lilists = document.getElementsByTagName('li');
+// const lilists = document.getElementsByTagName('li');
  
-for(const lilist of lilists){
-    console.log(lilist.innerText);
+// for(const lilist of lilists){
+//     console.log(lilist.innerText);
     
-}
-const headings = document.getElementsByTagName('h1');
-for(const heading of headings ){
-    console.log(heading.innerText);
+// }
+// getElementsByTagName
+// const headings = document.getElementsByTagName('h1');
+// for(const heading of headings ){
+//     console.log(heading.innerText);
+    
+// }
+
+// getElementById
+const place= document.getElementById('placeName'); 
+// console.log(place.innerText='Beautiful place');
+console.log(place.setAttribute('Title','Wow This is title'));
+
+
+// getElementsByClassName
+// const places= document.getElementsByClassName('place');
+// for(const pl of places){
+//     console.log(pl.innerHTML);
+    
+// }
+// querySelector
+const fruit=document.querySelector('.Fruits li')
+console.log(fruit);
+
+
+// querySelectorAll
+const fruits=document.querySelectorAll('.Fruits li');
+for(Fruit of fruits){
+    console.log(Fruit.innerHTML);
     
 }
