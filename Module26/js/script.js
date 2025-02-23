75765591a502472363f4b9828d9f2ac231d35b22
palsourov <palsourov365@gmail.com>
2025-02-23 17:11:52 +0600

    update module26

---
+++ Module26/js/script.js
@@ -0,0 +1,21 @@
+function bgYellow(){
+    
+    document.body.style.backgroundColor= "yellow";
+
+}
+function bgRed(){
+    
+    document.body.style.backgroundColor= 'red';
+
+}
+function bgRemove(){
+    
+    document.body.style.backgroundColor= '';
+
+}
+
+const bgBlue = document.getElementById('BgBlue');
+ bgBlue.onclick = function Blue(){
+    document.body.style.backgroundColor='blue';
+ }
+
