ccdfb71ce0f53134a64fe67b4f6050c4d627d1b5
palsourov <palsourov365@gmail.com>
2025-02-27 22:22:33 +0600

    add main.html file

--- Module27/js/script.js
+++ Module27/js/script.js
@@ -1,5 +1,31 @@
+ 
+//   login section
+
  document.getElementById("button").addEventListener('click',function(event){
     event.preventDefault();
-    console.log('hello');
-       
+   
+    const accountNumber= document.getElementById('Account').value ;
+    const password= document.getElementById('password').value ;
+    if(accountNumber.length ===11 ){
+        if(password.length===4) {
+           window.location.href = './main.html';
+        }
+        else{
+           if(password.length>4){
+              alert('Please enter 4 digit PIN');
+           }
+           else{
+              alert('Please fill up 4 digit PIN')
+           }
+        }
+    }
+    else{
+        if(accountNumber.length>11){
+            alert('Please ensure enter 11 digit number');
+        }
+        else{
+            alert('Please fill up 11 digit number');
+        }
+    }
+         
 })
