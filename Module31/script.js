fdaa73b39c81017ffc877767cb869e4d076d95e6
palsourov <palsourov365@gmail.com>
2025-03-06 18:02:32 +0600

    upload module31

---
+++ Module31/script.js
@@ -0,0 +1,12 @@
+//  mapping
+
+const arr=[2 ,3,4,5,6];
+console.log(arr);
+
+// const  newarry= arr.map(value=>value+5);
+// console.log(newarr);
+
+
+ const newarry = arr.map(Number=>Number*Number);
+ console.log(newarry);
+ 
